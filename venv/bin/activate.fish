c99c60ae0396da6cbfb80a6a5e626f1198ae8de8
albercm30 <albertocm30.2001@gmail.com>
2022-12-07 22:33:10 +0100

    had to delete and create again venv

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/var/home/lab/alum1/04/91/752/Descargas/TP/venv"
+set -gx VIRTUAL_ENV "/var/home/lab/alum0/04/14/242/zoowebapp/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
