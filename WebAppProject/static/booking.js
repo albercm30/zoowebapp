1f1e7c71636354ef6e140417dedeefe63f7df47b
omarDrame <hournice365@gmail.com>
2022-12-08 18:01:56 +0100

    booking in process

--- WebAppProject/static/booking.js
+++ WebAppProject/static/booking.js
@@ -1,10 +1,20 @@
 
 $(window).on("load", function() {
+    /*
     function myFunc(vars) {
         console.log(vars);
         return vars
     };
     myFunc(act_dates);
-
-   
+    */
+    var act_booked = $("#act_booked").html();
+    console.log(act_booked);
+    for (act in act_dates) {
+        console.log(act);
+        if (act === act_booked){
+            var option = $("<option>").html(act_dates[act]);
+            $("#date_chosen").append(option);
+        } 
+        
+    } 
 });
