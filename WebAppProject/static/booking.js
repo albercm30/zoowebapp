ff97f843d054c852129b46bac33efd7d63fbc03e
omarDrame <hournice365@gmail.com>
2022-12-08 20:08:23 +0100

    Works but overcoplicated

--- WebAppProject/static/booking.js
+++ WebAppProject/static/booking.js
@@ -9,22 +9,24 @@ $(window).on("load", function() {
     */
     //var act_booked = $("#act_booked");
 
-    
-        
+
     for (act in act_dates) {
         var reference_id = act_id[act];
         var node = $("#" + reference_id);
-        node.prop("checked", true);
-        
-        if (node.is(':checked')){
-            console.log(node);
-            for (activity in act_dates) {
-                var option = $("<option>").html(act_dates[activity]);
-                $("#date_chosen").append(option);
+
+        node.click(function(){
+            if (node.is(':checked')){
+                console.log(node);
+                for (activity in act_dates) {
+                    var option = $("<option>").html(act_dates[activity]);
+                    $("#date_chosen").append(option);
+                } 
+                
+                
             } 
-            
-            
-        } 
+        });
+
+        
         
     } 
             
