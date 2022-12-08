a589a2f6f9117229311637ac3bbbf57706c7bcb5
omarDrame <hournice365@gmail.com>
2022-12-08 21:00:22 +0100

    bug booking

--- WebAppProject/static/booking.js
+++ WebAppProject/static/booking.js
@@ -9,24 +9,32 @@ $(window).on("load", function() {
     */
     //var act_booked = $("#act_booked");
 
+    function displayDates(node,node_target,reference_id,id) {
+        node.click(function(){
+                for (activity in act_dates) {
+                    if (act_id[activity]===reference_id){
+                        var limit = act_dates[activity].length;
+                        for (let i = 0; i < limit; i++) {
+                            var option = $("<option>").html(act_dates[activity][i] );
+                            node_target.append(option);
+                        };
+                    } ;
+                    
+ 
+                };       
+        });
+    };
+
+
 
     for (act in act_dates) {
+        var id = act_id[act];
         var reference_id = act_id[act];
-        var node = $("#" + reference_id);
-
-        node.click(function(){
-            if (node.is(':checked')){
-                console.log(node);
-                for (activity in act_dates) {
-                    var option = $("<option>").html(act_dates[activity]);
-                    $("#date_chosen").append(option);
-                } 
-                
-                
-            } 
-        });
-
+        var node_source = $("#r" + reference_id);
+        var node_target =  $("#d" + reference_id);
+        displayDates(node_source,node_target,reference_id,id)
         
+
         
     } 
             
