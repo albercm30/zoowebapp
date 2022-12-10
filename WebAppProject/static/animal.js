b0a82aff56a57cedba63f124150d15560505a83f
albercm30 <albertocm30.2001@gmail.com>
2022-12-10 18:22:47 +0100

    js for animal

---
+++ WebAppProject/static/animal.js
@@ -0,0 +1,55 @@
+function showText(text){
+    document.getElementById("text-camel").innerHTML=text;
+}
+function hide(){
+    document.getElementById("text-camel").innerHTML="";
+}
+
+function showText2(text){
+    document.getElementById("text-snake").innerHTML=text;
+}
+function hide2(){
+    document.getElementById("text-snake").innerHTML="";
+}
+
+function showText3(text){
+    document.getElementById("text-stingray").innerHTML=text;
+}
+function hide3(){
+    document.getElementById("text-stingray").innerHTML="";
+}
+
+function showText4(text){
+    document.getElementById("text-dolphin").innerHTML=text;
+}
+function hide4(){
+    document.getElementById("text-dolphin").innerHTML="";
+}
+
+function showText5(text){
+    document.getElementById("text-polar-bear").innerHTML=text;
+}
+function hide5(){
+    document.getElementById("text-polar-bear").innerHTML="";
+}
+
+function showText6(text){
+    document.getElementById("text-penguin").innerHTML=text;
+}
+function hide6(){
+    document.getElementById("text-penguin").innerHTML="";
+}
+
+function showText7(text){
+    document.getElementById("text-deer").innerHTML=text;
+}
+function hide7(){
+    document.getElementById("text-deer").innerHTML="";
+}
+
+function showText8(text){
+    document.getElementById("text-falcon").innerHTML=text;
+}
+function hide8(){
+    document.getElementById("text-falcon").innerHTML="";
+}
