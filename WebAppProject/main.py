f31553a846a703bbf5c29b998be6e072fc87753e
omarDrame <hournice365@gmail.com>
2022-12-10 01:14:29 +0100

    booking in process

--- WebAppProject/main.py
+++ WebAppProject/main.py
@@ -4,6 +4,7 @@ import flask_login
 from flask_login import current_user
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from . import db, bcrypt
+from datetime import datetime
 
 
 from . import model
@@ -14,8 +15,8 @@ bp = Blueprint("main", __name__)
 # @flask_login.login_required
 def index():
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
-    return render_template("main/index.html",activities=activities,cheduleds=cheduleds)
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    return render_template("main/index.html",activities=activities,scheduleds=scheduleds)
 
 @bp.route("/animals")
 # @flask_login.login_required
@@ -26,71 +27,78 @@ def animals():
 @flask_login.login_required
 def customers():
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
     # for each activity title let's associate all of its available dates from the scheduled activities
     act_dates ={}
     #Same but with id of the activity as a key
     act_id = {}   
     for activity in activities:
-        for scheduled in cheduleds:
+        for scheduled in scheduleds:
             if activity.id == scheduled.activity_id:
                 if activity.is_marked:
                     act_dates[activity.title]=[] 
                     act_id[activity.title]=[] 
 
     for activity in activities:
-        for scheduled in cheduleds:
+        for scheduled in scheduleds:
             if activity.id == scheduled.activity_id:
                 if activity.is_marked:
                     act_dates[activity.title].append(scheduled.date)
                     act_id[activity.title]= activity.id
-    return render_template("main/customer.html",activities=activities,cheduleds=cheduleds,act_dates=act_dates,act_id=act_id)
+    return render_template("main/customer.html",activities=activities,scheduleds=scheduleds,act_dates=act_dates,act_id=act_id)
 
 @bp.route("/customer",methods=["POST"])
 @flask_login.login_required
 def booking():
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
 
     # for each activity title let's associate all of its available dates from the scheduled activities
     act_dates ={}
     #Same but with id of the activity as a key
     act_id = {}   
     for activity in activities:
-        for scheduled in cheduleds:
+        for scheduled in scheduleds:
             if activity.id == scheduled.activity_id:
                 if activity.is_marked:
                     act_dates[activity.title]=[] 
                     act_id[activity.title]=[] 
 
     for activity in activities:
-        for scheduled in cheduleds:
+        for scheduled in scheduleds:
             if activity.id == scheduled.activity_id:
                 if activity.is_marked:
                     act_dates[activity.title].append(scheduled.date)
                     act_id[activity.title]= activity.id
-
+    
     activity_title_booked = request.form.get("activity_title_booked")
-    activity = model.Scheduledactivity.query.filter_by(title=activity_title_booked).first()
+    scheduled_date = request.form.get("date_chosen")
+    activity = model.Activity.query.filter_by(title=activity_title_booked).first()
     activity_id = activity.id
+    print("scheduled_date",type(scheduled_date))
+    scheduled = model.Scheduledactivity.query.filter_by(activity_id=activity_id,date=scheduled_date).first()
     places_booked = request.form.get("places_booked")
-    date_time = datetime.datetime.now(dateutil.tz.tzlocal())
-    if places_booked > activity.places:
-        flash("There are no more available places")
+    places_booked = int(places_booked)
+    print(activity_title_booked,scheduled_date,places_booked)
+    date_time = datetime.now(dateutil.tz.tzlocal())
+    
+    if places_booked > scheduled.places:
+        flash("There not enough available places")
     else :
-        new_reservation = model.Reservation(user_id=current_user,activity_id=activity_id,places=places_booked,
+        new_reservation = model.Reservation(user_id=current_user.id,activity_id=scheduled.activity_id,places=places_booked,
         date=date_time)
         db.session.add(new_reservation)
-        setattr(activity, 'places', activity.places - places_booked)
+        setattr(scheduled, 'places', scheduled.places - places_booked)
         db.session.commit()
-    return render_template("main/customer.html",activities=activities,act_dates=act_dates,scheduleds=cheduleds,act_id=act_id)
+        flash("Your reservation is successful")
+    return render_template("main/customer.html",activities=activities,act_dates=act_dates,scheduleds=scheduleds,act_id=act_id)
 
 @bp.route("/activity")
 # @flask_login.login_required
 def activities():
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
-    return render_template("main/activity.html",activities=activities, cheduleds=cheduleds)
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    return render_template("main/activity.html",activities=activities, scheduleds=scheduleds)
 
 @bp.route("/activity/",methods=["POST"])
 @flask_login.login_required
@@ -103,8 +111,8 @@ def add_activity():
     db.session.commit()
     flash("You've successfully added an activity")
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
-    return render_template("main/activity.html",activities=activities, cheduleds=cheduleds)
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    return render_template("main/activity.html",activities=activities, scheduleds=scheduleds)
 
 @bp.route("/activitysched",methods=["POST"])
 @flask_login.login_required
@@ -125,8 +133,8 @@ def add_schedule():
     db.session.commit()
     flash("You've successfully added scheduled the activityy")
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
-    return render_template("main/activity.html",activities=activities, cheduleds=cheduleds)
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    return render_template("main/activity.html",activities=activities, scheduleds=scheduleds)
 
     
 @bp.route("/activitymark",methods=["POST"])
@@ -144,5 +152,5 @@ def mark_activity():
     db.session.commit()
     flash("You've successfully added scheduled the activityy")
     activities = model.Activity.query.order_by(model.Activity.id.desc()).all()
-    cheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
-    return render_template("main/activity.html",activities=activities, cheduleds=cheduleds)
+    scheduleds = model.Scheduledactivity.query.order_by(model.Scheduledactivity.id.desc()).all()
+    return render_template("main/activity.html",activities=activities, scheduleds=scheduleds)
